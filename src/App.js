a20fcdd42d91cc2f241f294d52e014cf0edb5a3e
Edwin Pile <epilepra@gmail.com>
2023-01-10 13:51:38 -0500

    finished version v1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/system";
+import { Feed, Navbar, VideoDetail, ChannelDetail, SearchFeed } from './components';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/youtube_clone/" exact element={<Feed />} />
+          <Route path="/youtube_clone/video/:id" element={<VideoDetail />} />
+          <Route
+            path="/youtube_clone/channel/:id"
+            element={<ChannelDetail />}
+          />
+          <Route
+            path="/youtube_clone/search/:searchTerm"
+            element={<SearchFeed />}
+          />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
